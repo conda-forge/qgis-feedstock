f7e498b51c9f1638fdae1dd3944662e1bc8a801c
Chris Holden <ceholden@gmail.com>
2019-03-26 10:47:27 -0400

    Add missing OSX activate env vars, & reorganize

--- recipe/scripts/activate.sh
+++ recipe/scripts/activate.sh
@@ -2,29 +2,39 @@
 
 # Store existing QGIS env vars and set to this conda env
 # so other QGIS installs don't pollute the environment
-
+if [[ -n "$QGIS_PREFIX_PATH" ]]; then
+    export _CONDA_SET_QGIS_PREFIX_PATH=$QGIS_PREFIX_PATH
+fi
 if [[ -n "$PYTHONPATH" ]]; then
     export _CONDA_SET_PYTHONPATH=$PYTHONPATH
 fi
+if [[ -n "$QT_PLUGIN_PATH" ]]; then
+    export _CONDA_SET_QT_PLUGIN_PATH=$QT_PLUGIN_PATH
+fi
+# Mac specific
+if [[ -n "$QGIS_BUNDLE" ]]; then
+    export _CONDA_SET_QGIS_BUNDLE=$QGIS_BUNDLE
+fi
+
 
 if [ -d $CONDA_PREFIX/share/qgis/python ]; then
-    # On Linux
-    export PYTHONPATH="$CONDA_PREFIX/share/qgis/python:$CONDA_PREFIX/share/qgis/python/plugins:$PYTHONPATH"
+    # Linux only needs to be able to find Python packages
+    export PYTHONPATH="$CONDA_PREFIX/share/qgis/python:$PYTHONPATH"
+    export PYTHONPATH="$CONDA_PREFIX/share/qgis/python/plugins:$PYTHONPATH"
 elif [ -d $CONDA_PREFIX/QGIS.app ]; then
-    # On MacOS
-    export PYTHONPATH="$CONDA_PREFIX/QGIS.app/Contents/Resources/python:$CONDA_PREFIX/QGIS.app/Contents/Resources/python/plugins:$PYTHONPATH"
+    # MacOS
+    # See: https://github.com/OSGeo/homebrew-osgeo4mac/blob/master/Formula/qgis-ltr.rb#L895
+    export QGIS_BUNDLE="$CONDA_PREFIX/QGIS.app/Contents"
+    export QGIS_PREFIX_PATH="$CONDA_PREFIX/QGIS.app/Contents/MacOS"
+    export PYTHONPATH="$CONDA_PREFIX/QGIS.app/Contents/Resources/python:$PYTHONPATH"
+    export PYTHONPATH="$CONDA_PREFIX/QGIS.app/Contents/Resources/python/plugins:$PYTHONPATH"
+    export QT_PLUGIN_PATH="$CONDA_PREFIX/plugins:$QT_PLUGIN_PATH"
 elif [ -d $CONDA_PREFIX/Library/python ]; then
     # On Windows
-    export PYTHONPATH="$CONDA_PREFIX/Library/python:$CONDA_PREFIX/Library/python/plugins:$PYTHONPATH"
-
-    # Windows needs more paths...
-    if [[ -n "$QGIS_PREFIX_PATH" ]]; then
-        export _CONDA_SET_QGIS_PREFIX_PATH=$QGIS_PREFIX_PATH
-    fi
+    # See: https://github.com/qgis/QGIS/blob/master/ms-windows/osgeo4w/qgis.vars
     export QGIS_PREFIX_PATH=$CONDA_PREFIX/Library
-
-    if [[ -n "$QT_PLUGIN_PATH" ]]; then
-        export _CONDA_SET_QT_PLUGIN_PATH=$QT_PLUGIN_PATH
-    fi
-    export QT_PLUGIN_PATH="$CONDA_PREFIX/Library/plugins:$CONDA_PREFIX/Library/qtplugins;$QT_PLUGIN_PATH"
+    export PYTHONPATH="$CONDA_PREFIX/Library/python:$PYTHONPATH"
+    export PYTHONPATH="$CONDA_PREFIX/Library/python/plugins:$PYTHONPATH"
+    export QT_PLUGIN_PATH="$CONDA_PREFIX/Library/plugins:$QT_PLUGIN_PATH"
+    export QT_PLUGIN_PATH="$CONDA_PREFIX/Library/qtplugins;$QT_PLUGIN_PATH"
 fi
--- recipe/scripts/deactivate.sh
+++ recipe/scripts/deactivate.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 # Restore previous QGIS env var if it exists
 
+unset QGIS_PREFIX_PATH
+if [[ -n "$_CONDA_SET_QGIS_PREFIX_PATH" ]]; then
+    export QGIS_PREFIX_PATH="$_CONDA_SET_QGIS_PREFIX_PATH"
+    unset _CONDA_SET_QGIS_PREFIX_PATH
+fi
 
 unset PYTHONPATH
 if [[ -n "$_CONDA_SET_PYTHONPATH" ]]; then
@@ -8,16 +13,13 @@ if [[ -n "$_CONDA_SET_PYTHONPATH" ]]; then
     unset _CONDA_SET_PYTHONPATH
 fi
 
+if [[ -n "$_CONDA_SET_QT_PLUGIN_PATH" ]]; then
+    export QT_PLUGIN_PATH="$_CONDA_SET_QT_PLUGIN_PATH"
+    unset _CONDA_SET_QT_PLUGIN_PATH
+fi
 
-# Windows has more
-if [ -d $CONDA_PREFIX/Library/python ]; then
-    unset QGIS_PREFIX_PATH
-    if [[ -n "$_CONDA_SET_QGIS_PREFIX_PATH" ]]; then
-        export QGIS_PREFIX_PATH="$_CONDA_SET_QGIS_PREFIX_PATH"
-        unset _CONDA_SET_QGIS_PREFIX_PATH
-    fi
-    if [[ -n "$_CONDA_SET_QT_PLUGIN_PATH" ]]; then
-        export QT_PLUGIN_PATH="$_CONDA_SET_QT_PLUGIN_PATH"
-        unset _CONDA_SET_QT_PLUGIN_PATH
-    fi
+unset QGIS_BUNDLE
+if [[ -n "$_CONDA_SET_QGIS_BUNDLE" ]]; then
+    export QGIS_BUNDLE="$_CONDA_SET_QGIS_BUNDLE"
+    unset _CONDA_SET_QGIS_BUNDLE
 fi
