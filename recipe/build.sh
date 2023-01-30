0bd660146bc7867eecf9f742772875136e3ca020
Sam Gillingham <gillingham.sam@gmail.com>
2023-01-30 11:49:05 +1000

    add PKG_CONFIG back

--- recipe/build.sh
+++ recipe/build.sh
@@ -40,7 +40,7 @@ fi
 
 if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" == "1" ]]; then
     # ensure we find the correct pkg-config during cross compilation
-    #export PKG_CONFIG=${BUILD_PREFIX}/bin/pkg-config
+    export PKG_CONFIG=${BUILD_PREFIX}/bin/pkg-config
     #export BISON_ROOT=${BUILD_PREFIX}
     #export FLEX_ROOT=${BUILD_PREFIX}
     #export PDAL_ROOT=${BUILD_PREFIX}
