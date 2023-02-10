b34cbc60b33ddb153e54d74b6dcc35125e8786a1
Sam Gillingham <gillingham.sam@gmail.com>
2023-02-10 20:09:01 +0000

    create our own sip-build

--- recipe/build.sh
+++ recipe/build.sh
@@ -43,14 +43,19 @@ if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" == "1" ]]; then
     export PKG_CONFIG=${BUILD_PREFIX}/bin/pkg-config
     export BISON_ROOT=${BUILD_PREFIX}
     export FLEX_ROOT=${BUILD_PREFIX}
-    #export PDAL_ROOT=${BUILD_PREFIX}
     # hide this so we get the arm libs but x86 protoc
     rm ${PREFIX}/bin/protoc
     rm ${PREFIX}/bin/pdal
     rm ${PREFIX}/bin/sip-build
-    # to find m4/pdal
+    # to find m4
     export M4=${BUILD_PREFIX}/bin/m4
-    #export PATH=${BUILD_PREFIX}/bin:${PATH}
+    
+    # make our own sip-build with correct settings (uses PATH set above)
+    SITE_PKGS_PATH=$($PREFIX/bin/python -c 'import site;print(site.getsitepackages()[0])')
+    echo "#!/bin/bash" > sip-build
+    echo "$BUILD_PREFIX/bin/python -m sipbuild.tools.build --target-dir $SITE_PKGS_PATH $@" >> sip-build
+    chmod +x sip-build
+    cat sip-build
 fi
 
 # TODO: enable QSPATIALITE on OSX
