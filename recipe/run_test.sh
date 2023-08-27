8df0e62dad716dc48d1753fe6074bb7598711ec6
Mervin Fansler <mmfansler@gmail.com>
2023-08-26 20:04:49 -0400

    set offscreen earlier

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -5,6 +5,9 @@ set -e
 # print all commands
 set -x
 
+# tell Qt we don't have a display
+export QT_QPA_PLATFORM=offscreen
+
 # Testing QGIS command currently is found as lint in meta.yaml
 # QGIS has no --version, and --help exists 2
 qgis --help || [[ "$?" == "2" ]]
@@ -16,10 +19,6 @@ python -c 'import qgis.gui'
 python -c 'import qgis.utils'
 
 # Test actual use of Python API
-# First tell Qt we don't have a display
-export QT_QPA_PLATFORM=offscreen
-
-
 if [ $(uname) == Darwin ]; then
     set +e
     python test_py_qgis.py
